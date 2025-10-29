d90efb3392c06aff939d6f400ebf3ac4846e5c74
Emma <EmmaRussellKilburn@gmail.com>
2025-10-29 17:01:52 +0000

    project menu

---
+++ src/components/ProjectMenu.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { Listgroup } from "flowbite-svelte";
+	let itemList = ["Setup", "Download"];
+</script>
+
+<Listgroup items={itemList} class="w-48" />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Map from "../components/Map.svelte";
+		import ProjectMenu from '../components/ProjectMenu.svelte';
 </script>
-
 <Map></Map>
+<ProjectMenu></ProjectMenu>
