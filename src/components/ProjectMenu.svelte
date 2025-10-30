6fa673eba5ecf8ee56c17459d830ce2e09ef773b
Emma <EmmaRussellKilburn@gmail.com>
2025-10-30 11:05:06 +0000

    updated packages and added ProjectMenu

--- src/components/ProjectMenu.svelte
+++ src/components/ProjectMenu.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
-	import { Listgroup } from "flowbite-svelte";
-	let itemList = ["Setup", "Download"];
+	import { Listgroup } from 'flowbite-svelte';
+	import { CogOutline, DownloadOutline } from 'flowbite-svelte-icons';
+	import { ProjectDialog } from '../types';
+	import { store } from '../store.svelte.js';
+
+	let buttons = $derived([
+		{ Icon: CogOutline, dialog: ProjectDialog.Setup, disabled: !store.enabledProjectDialogs.has(ProjectDialog.Setup) },
+		{ Icon: DownloadOutline, dialog: ProjectDialog.Download, disabled: !store.enabledProjectDialogs.has(ProjectDialog.Download) }
+	]);
+	const handleClick = (e) => {
+		// toggle related dialog
+		const dialog = e.detail.dialog;
+		if (store.openProjetDialog === dialog) {
+			store.openProjetDialog = null;
+		} else if (store.enabledProjectDialogs.has(dialog)) {
+			store.openProjetDialog = dialog;
+		}
+	};
 </script>
 
-<Listgroup items={itemList} class="w-48" />
+<Listgroup active items="{buttons}" onclick={handleClick} class="absolute top-20 left-4 w-14 z-9999" />
